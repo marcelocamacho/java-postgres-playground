60c333f301ecd75b92d3129b5b6e9a98c6c26c79
Marcelo Camacho <marcelokmacho@gmail.com>
2023-08-21 14:53:28 +0000

    Concluída a aula 6

---
+++ src/main/java/com/aulas/Aula6_Lacos.java
@@ -0,0 +1,35 @@
+package com.aulas;
+
+import java.util.Arrays;
+
+public class Aula6_Lacos {
+    public static void main(String[] args) {
+// Laço for em vetor        
+        int vetor[] = new int[6];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = 100 * (i+1);
+        }
+
+        System.out.println(Arrays.toString(vetor));
+        System.out.println();
+//Laço for em matriz
+
+        double matriz[][] = new double[2][3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(i + "*" + matriz[i].length + "+" + j + "  ");
+
+                matriz[i][j] = i * matriz[i].length + j;
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.toString(matriz[0]));
+        System.out.println(Arrays.toString(matriz[1]));
+    
+
+
+    }
+}
