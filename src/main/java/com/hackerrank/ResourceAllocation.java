aa5ba04270e06f577e2c57ea49d747605313ecb0
Marcelo Camacho <marcelokmacho@gmail.com>
2023-09-16 15:50:33 +0000

    Novo desafio

---
+++ src/main/java/com/hackerrank/ResourceAllocation.java
@@ -0,0 +1,66 @@
+package com.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+5
+1
+8
+3
+9
+6
+7
+9
+6
+14
+7
+*/
+class Result {
+
+    public static int getMinMatchines(List<Integer> start, List<Integer> end) {
+    
+        //System.out.println(Arrays.toString(start.toArray()));
+        //System.out.println(Arrays.toString(end.toArray()));
+        return 3;
+    }
+}
+
+public class ResourceAllocation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        List<Integer> start = new ArrayList<>();
+        for (int nItr = 0; nItr < n; nItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+            int k = Integer.parseInt(firstMultipleInput[0]);
+            start.add(k);
+        }
+        List<Integer> end = new ArrayList<>();
+        for (int nItr = 0; nItr < n; nItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+            int k = Integer.parseInt(firstMultipleInput[0]);
+            end.add(k);
+        }
+        
+        bufferedReader.close();
+
+        System.out.println(Arrays.toString(start.toArray()));
+        System.out.println(Arrays.toString(end.toArray()));
+
+        int result = Result.getMinMatchines(start,end);
+
+        bufferedWriter.write("->"+result);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
